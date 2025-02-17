50f88eb69f60ca8ea567bf39ccce2344d562c7c2
Giuseppe <glupari100@gmail.com>
2025-02-17 13:51:50 +0000

    added tests for new math functions

--- lab1/test_math_functions.py
+++ lab1/test_math_functions.py
@@ -1,6 +1,21 @@
-from math_functions import add
+from math_functions import add, subtract, multiply, divide
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(5, 5) == 10
+
+def test_subtract():
+    assert subtract(3, 2) == 1
+    assert subtract(1, 1) == 0
+    assert subtract(15, 5) == 10
+
+def test_multiply():
+    assert multiply(3, 2) == 6
+    assert multiply(1, 1) == 1
+    assert multiply(-1, 5) == -5
+
+def test_multiply():
+    assert divide(4, 2) == 2
+    assert divide(10, 2) == 5
+    assert divide(1, -1) == -1
