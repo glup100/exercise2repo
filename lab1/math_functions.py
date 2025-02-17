961eb001f3c6a5175dcc40736a39f4927c27f5f7
Giuseppe <glupari100@gmail.com>
2025-02-17 13:28:46 +0000

    updates to math fuctions - added subtract, multiply, divide

--- lab1/math_functions.py
+++ lab1/math_functions.py
@@ -1,2 +1,21 @@
 def add(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+        return "Both inputs must be numbers."
     return x + y
+
+def subtract(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+        return "Both inputs must be numbers."
+    return x - y
+
+def multiply(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+        return "Both inputs must be numbers."
+    return x * y
+
+def divide(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
+        return "Both inputs must be numbers."
+    if y == 0:
+        return "Cannot divide by zero."
+    return x / y
