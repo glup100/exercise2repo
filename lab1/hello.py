edecedecebbb6664fd8e75cf37792024df3158aa
Giuseppe <glupari100@gmail.com>
2025-02-17 13:31:16 +0000

    updating the api - healthcheck endpoint

--- lab1/hello.py
+++ lab1/hello.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from math_functions import add
+from math_functions import add, subtract, multiply, divide
 import random
 
 app = FastAPI()
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World", "value": str(add(random.randint(0, 10), random.randint(11, 30)))}
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
