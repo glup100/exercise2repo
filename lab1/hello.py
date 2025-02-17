edc9513bce10468b0cd4fca8953235458f956365
Giuseppe <glupari100@gmail.com>
2025-02-17 13:40:33 +0000

    api enhancemnt - exception handling

--- lab1/hello.py
+++ lab1/hello.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from math_functions import add, subtract, multiply, divide
 import random
 
@@ -7,6 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World", "value": str(add(random.randint(0, 10), random.randint(11, 30)))}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/health")
 async def health_check():
